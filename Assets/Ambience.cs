664958a1ec087764170c12384c710bc0974ea1c2
VladimirShevelevich <vasiliy812@yandex.ru>
2018-02-02 23:57:10 +0200

    Planets added

---
+++ Assets/Ambience.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ambience : MonoBehaviour {
+
+    public GameObject[] planets;
+
+    int index;
+
+    IEnumerator CreationPlanets()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(10);
+            CreatePlanet();
+        }
+    }
+
+    void CreatePlanet()
+    {
+        Instantiate(planets[index], planets[index].transform.position, Quaternion.identity);
+        index++;
+        if (index >= planets.Length)
+            index = 0;
+    }
+}
